1997a2fbfc31077c661124771a427e6e88cd2d65
Kuzan19 <levan.kanashyan@gmail.com>
2024-01-09 17:53:47 +0300

    fixing commit

--- newapp/urls.py
+++ newapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.TableView.as_view(), name='table-views-page'),
+    path('sort_by_name', views.SortedTableView.as_view(), name='sorted-name-table-views-page'),
+    path('sort_by_age', views.SortedTableView.as_view(), name='sorted-age-table-views-page'),
     # path('create_new_strings_database', views.new_data_base),
 ]
--- newapp/views.py
+++ newapp/views.py
@@ -11,10 +11,18 @@ class TableView(ListView):
     context_object_name = 'users'
     paginate_by = 20
 
-    def get_ordering(self):
-        """Функция сортировки по переданному атрибуту 'orderby' """
-        ordering = self.request.GET.get('orderby')
-        return ordering
+    def get_context_data(self, *args, **kwargs):
+        """Прокидывание данных о фильтрации и сортировке в шаблон"""
+        context = super().get_context_data(*args, **kwargs)
+        if self.request.GET.get('orderby'):
+            context['ordering'] = self.request.GET.get('orderby')
+        else:
+            context['ordering'] = 'id'
+        if self.request.GET.get('filter_by'):
+            context['filtering'] = self.request.GET.get('filter_by')
+        else:
+            context['filtering'] = ''
+        return context
 
     def get_queryset(self):
         """Функция фильтрации под переданному атрибуту 'filter_by', если он передается"""
@@ -23,6 +31,25 @@ class TableView(ListView):
             queryset = UserModel.objects.filter(gender=filtering)
         else:
             queryset = UserModel.objects.all()
+        return queryset
+
+
+class SortedTableView(TableView):
+    """Функция отображения отсортированной таблицы"""
+    def get_queryset(self):
+        """Функция фильтрации под переданному атрибуту 'filter_by', если он передается"""
+        if self.request.path == '/sort_by_name':
+            if self.request.GET.get('filter_by'):
+                filtering = self.request.GET.get('filter_by')
+                queryset = UserModel.objects.filter(gender=filtering).order_by('name')
+            else:
+                queryset = UserModel.objects.all().order_by('name')
+        elif self.request.path == '/sort_by_age':
+            if self.request.GET.get('filter_by'):
+                filtering = self.request.GET.get('filter_by')
+                queryset = UserModel.objects.filter(gender=filtering).order_by('age')
+            else:
+                queryset = UserModel.objects.all().order_by('age')
         return queryset
 
 
